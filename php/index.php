d59722334102a32fcfc718e49dc8d524662dee31
C-jimenez21 <cristianjjc21@gmail.com>
2023-06-13 13:17:11 -0500

    feat: :sparkles: Se añade el metodo SearchID y los metodosdel CRUD ELIMINAR y EDITAR
    Se desarrolla el codigo para la implementacion de nuevas funciones del crud tales como eliminar y editar ademas de la preparacion de la funcion que trae al informacion

--- php/index.php
+++ php/index.php
@@ -1,6 +1,6 @@
 <?php
     $URL = "https://6480e3aef061e6ec4d4a000d.mockapi.io/Informacion";
-
+    
     //validar cedula
     if(isset($_POST["cedula"])){
         
@@ -11,77 +11,108 @@
             if($items === ""){
                 echo "<h1>SE DEBEN LLENAR TODOS LOS ESPACIOS</h1>";
                 return false;
-        }else{
-            return true;
+            }else{
+                return true;
+            }
         }
-    }
         
     }
     //existe? 
-
+    
     //se pudo crear
-
-
+    
+    
     //agregar
     function addDAta($data){
         if(validateInput($data)){
             global $URL;
             $opts = array(
                 'http'=>array(
-                  'method'=>"POST",
+                    'method'=>"POST",
                   'header'=>"Content-type: application/json",
                   "content" => json_encode($data)
                   )
                 );
-            $config = stream_context_create($opts);
-            return file_get_contents($URL, false, $config);
-            }
+                $config = stream_context_create($opts);
+            return $DATA = file_get_contents($URL, false, $config);
+        }
         
     }
-
+    
     //editar
-
-
-    //eliminar
-
-
-    //buscar
-
-
-    //Switch para saber que funcion es
-
-    if(isset($_POST["btn"])){
-        $accion = $_POST["btn"];
-        $datos = $_POST;
-        switch($accion){
-            case "confirmar":
-                addDAta($datos);
-                break;
-            case "eliminar":
-                break;
-            case "buscar":
-                break;
-            case "editar":
-                break;
+    function searchID($cedula){
+        global $URL;
+        $result = file_get_contents($URL);
+        $user = json_decode($result, true);
+        for($i = 0; $i<= count($user);$i++){
+            if($cedula == $user[$i]["Cedula"]){
+                return $user[$i];
+            }
         }
     }
-
-
-
-
-
-
-
-    /*$opts = array(
-        'http'=>array(
-          'method'=>"GET",
-          'header'=>"Content-type: application/json",
-          "content" => json_encode($data)
+    //eliminar
+    function deleteData($cedula){
+        $usuario = searchID($cedula);
+        echo "<h1>$newURL</h1>";
+        $opts = array(
+            'http'=>array(
+                "method"=> "DELETE"
+                )
+            );
+            $config = stream_context_create($opts);
+            return $DATA = file_get_contents($newURL, false, $config);
+        }
+    //Editar
+    function editUser($cedula){
+        $usuario = searchID($cedula);
+        $newURL = "https://6480e3aef061e6ec4d4a000d.mockapi.io/Informacion/".$usuario["id"];
+        $opts = array(
+            'http'=>array(
+                'method'=>"PUT",
+              'header'=>"Content-type: application/json",
+              "content" => json_encode($cedula)
+              )
+            );
+            $config = stream_context_create($opts);
+        return $DATA = file_get_contents($newURL, false, $config);
+    }
+        
+        //Switch para saber que funcion es
+        if(isset($_POST["btn"])){
+            echo $_POST["btn"];
+            $accion = $_POST["btn"];
+            $datos = $_POST;
+            switch($accion){
+                case "confirmar":
+                    addDAta($datos);
+                    break;
+                    case "eliminar":
+                        deleteData($_POST["Cedula"]);
+                        break;
+                        case "buscar":
+                            $getInfo = editUser($_POST["Cedula"]);
+                            break;
+            case "editar":
+                break;
+            }
+        }
+        
+        
+        
+        
+        
+        $DATA = file_get_contents($URL);
+        
+        
+        /*$opts = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>"Content-type: application/json",
+                "content" => json_encode($data)
         )
       );
-    $config = stream_context_create($opts);
+      $config = stream_context_create($opts);
     */
-    $DATA = file_get_contents($URL);
 ?>
 
 
@@ -99,20 +130,20 @@
 </head>
 <body>
     <div class="container mt-5">
-    <form>
+    <form method = "POST" >
         <div class="row one">
             <div class="col-6">
                 <div class="mb-3">
                     <label class="form-label">Nombre</label>
-                    <input type="text" name="Nombre" placeholder="Ingrese nombre">
+                    <input type="text" name="Nombre" placeholder="Ingrese nombre" value ="<?php if(isset($getInfo["Nombre"])) ? print $getInfo["Nombre"] : print "" ?>">
                 </div>
                 <div class="mb-3">
                   <label class="form-label">Apellido</label>
-                  <input type="text" name="Apellido" placeholder="Ingrese apellido">
+                  <input type="text" name="Apellido" placeholder="Ingrese apellido"value ="<?php if(isset($getInfo["Apellido"])) ? print $getInfo["Apellido"] : print "" ?>">
                 </div>
                 <div class="mt-4">
                   <label class="form-label">Direccion</label>
-                  <input type="text" name="Direccion" placeholder="Ingrese direccion">
+                  <input type="text" name="Direccion" placeholder="Ingrese direccion" value ="<?php if(isset($getInfo["Direccion"])) ? print $getInfo["Direccion"] : print "" ?>">
                 </div>
             </div>
             <div class="col-6">
@@ -120,11 +151,11 @@
                 <!-- <h1 class="display-3">Campuslands</h1> -->
                 <div class="mb-3 mt-3">
                   <label class="form-label">Edad</label>
-                  <input type="number" name="Edad" placeholder="Ingrese edad">
+                  <input type="number" name="Edad" placeholder="Ingrese edad" value ="<?php if(isset($getInfo["Edad"])) ? print $getInfo["Edad"] : print "" ?>">
                 </div>
                 <div class="mb-3">
                   <label class="form-label">Email</label>
-                  <input type="email" name="Email" placeholder="Ingrese email">
+                  <input type="email" name="Email" placeholder="Ingrese email" value ="<?php if(isset($getInfo["Email"])) ? print $getInfo["Email"] : print "" ?>">
                 </div>
             </div>
         </div>
@@ -132,41 +163,38 @@
         <div class="row">
             <div class="col-6 mb-3">
                 <label class="form-label">Horario</label>
-                <input type="time" name="Hora">
+                <input type="time" name="Hora" value ="<?php if(isset($getInfo["Horario"])) ? print $getInfo["Horario"] : print "" ?>">
             </div>
             <div class="col"></div>
             <div class="col-1">
-                <button type="submit" name= "btn" value="confirmar">&#10004;</button>
+                <button type="submit" name="btn" value="confirmar">&#10004;</button>
             </div>
             <div class="col"></div>
             <div class="col-1">
-                <button type="submit" name= "btn" value="eliminar">&#10006;</button>
+                <button type="submit" name="btn" value="eliminar">&#10006;</button>
             </div>
             <div class="col"></div>
         </div>
         <div class="row">
             <div class="col-6 mb-3">
                 <label class="form-label">Team</label>
-              <input type="text" name="Team" placeholder="Ingrese team">
+              <input type="text" name="Team" placeholder="Ingrese team" value ="<?php if(isset($getInfo["Team"])) ? print $getInfo["Team"] : print "" ?>">
            </div>
            <div class="col"></div>
             <div class="col-1">
-                <button type="submit" name= "btn" value="editar">&#9999;</button>
+                <button type="submit" name="btn" value="editar">&#9999;</button>
             </div>
             <div class="col"></div>
             <div class="col-1">
-                <button type="submit" name= "btn" value="buscar">&#128269;</button>
+                <button type="submit" name="btn" value="buscar">&#128269;</button>
             </div>
-    //editar
-
-
-    //eliminar
+    
             <div class="col"></div>
         </div>
         <div class="row">
             <div class="col-6 mb-3">
               <label class="form-label">Trainer</label>
-              <input type="text" name="Trainer" placeholder="Ingrese trainer">
+              <input type="text" name="Trainer" placeholder="Ingrese trainer" value ="<?php if(isset($getInfo["Trainer"])) ? print $getInfo["Trainer"] : print "" ?>">
             </div>
             <div class="col-6 mb-3">
               <label class="form-label">Cedula</label>
